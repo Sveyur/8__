54e3c0d4a68fbde59245d537a7b8769ab8ec74bb
Sveta <Sveta@KOMP>
2021-11-27 23:10:23 +0300

    Добавьте файлы проекта.

---
+++ 8__/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _8__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Sveta\source\repos\8__\8__";
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string d in dirs)
+            {
+                Console.WriteLine("\nКаталог: {0}", d);
+                string[] dir = Directory.GetDirectories(d);
+                foreach (string dv in dir)
+                {
+                    Console.WriteLine("    Подкаталог: {0}", dv);
+                    string[] di = Directory.GetFiles(dv);
+                    foreach (string ssss in di)
+                    {
+                        Console.WriteLine("          Файл подкаталога: " + ssss);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Файлы:");
+            string[] files = Directory.GetFiles(path);
+            foreach (string d in files)
+            {
+                Console.WriteLine(d);
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ 8__2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _8__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = @"C:\Users\Sveta\source\repos\8__\8__2\bin\Debug\new papka";
+            string path2 = @"C:\Users\Sveta\source\repos\8__\8__2\bin\Debug\new papka\txt.txt";
+            Random random = new Random();
+            StreamWriter sw = new StreamWriter(path2, false);
+
+            if (!Directory.Exists(path1))
+            {
+                Directory.CreateDirectory(path1);
+            }
+            if (!File.Exists(path2))
+            {
+                File.Create(path2);
+            }
+            for (int a = 0; a < 10; a++)
+            {
+                sw.WriteLine("{0}", random.Next(0, 10));
+            }
+            sw.Close();
+            StreamReader sr = new StreamReader(path2);
+            string s;
+            int summ = 0;
+            while ((s = sr.ReadLine()) != null)
+            {
+                summ += Convert.ToInt32(s);
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Сумма чисел = " + summ);
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
+
